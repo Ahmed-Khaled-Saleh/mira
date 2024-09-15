0f548de7261a1387b9791b6e12d7b874da519e6c
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-09-15 18:58:38 +0300

    added more hours for the exp

--- jobs/fedk.sh
+++ jobs/fedk.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=100G
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
 #SBATCH --gres=gpu:a100:1
 #SBATCH --output=logs/fedkseed.out
 #SBATCH --error=logs/fedkseed.err
