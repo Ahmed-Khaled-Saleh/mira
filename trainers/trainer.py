07d2d483ba6fc5cf5103bdb5ba721745359c60b1
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-09-17 14:04:56 +0300

    added debugging in trainer

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -28,7 +28,7 @@ class Trainer:
         def closure():
             out = self.client.model(**batch)
             return self.client.criterion(out)
-
+        import ipdb; ipdb.set_trace()
         if self.client.args.name in ['fedk', 'mira']:
             loss, zo_random_seed, projected_grad = self.client.optimizer.step(closure)
             self.client._add_seed_pole(zo_random_seed, projected_grad)
