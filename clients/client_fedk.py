81ea9992a0054fd08d8bf740ace6c68433a352fe
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-09-17 04:20:00 +0300

    sync

--- clients/client_fedk.py
+++ clients/client_fedk.py
@@ -47,7 +47,11 @@ class Client_fedk(BaseClient):
         self.candidate_seeds = candidate_seeds
         self.local_seed_pool = {seed: 0.0 for seed in self.candidate_seeds}
 
-        self.task = self.train_ds[0]['task']
+        if self.args.data == 'dolly':
+            self.task = self.train_ds[0]['category']
+        else:
+            self.task = self.train_ds[0]['task']
+            
         self.task = self.task if isinstance(self.task, str) else self.task[0]
         self.train_stat = {}
         self.test_stats = {}
