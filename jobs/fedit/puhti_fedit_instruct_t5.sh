42a96386362e91060c9fbd3822943bdc5d596a21
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-09-18 21:08:36 +0300

    added job files for instruct_gpt2+fedit

---
+++ jobs/fedit/puhti_fedit_instruct_t5.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --account=project_2009050
+#SBATCH --job-name=puhti_fedit_t5_instruct
+#SBATCH --partition=gpu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=100G
+#SBATCH --time=72:00:00
+#SBATCH --gres=gpu:v100:1
+#SBATCH --output=logs/puhti_fedit_t5_instruct.out
+#SBATCH --error=logs/puhti_fedit_t5_instruct.err
+
+module --force purge
+module load pytorch
+source /projappl/project_2009050/python_envs/torch/bin/activate
+cd /projappl/project_2009050/code/mira
+
+export PYTHONPATH=$PYTHONPATH:/projappl/project_2009050/python_envs/torch/lib/python3.9/site-packages
+echo "Current PYTHONPATH: $PYTHONPATH"
+python -c "import rouge; print('rouge module is installed and importable')"
+
+srun python main.py --fname ./configs/fedit/configs_fedit_instruct_t5.yaml
