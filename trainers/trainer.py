b45205803c851d5640a6a02ba5323d9cf40cef9b
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-09-18 17:38:59 +0300

    more debugging

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -172,6 +172,8 @@ class Trainer:
         def _run_batch(batch):
             print("BATCH:**")
             print(batch)
+            print(self.client.model)
+            print(self.client.model.device)
             out = self.client.model(**batch)
             loss = self.client.criterion(out)
             if torch.isnan(loss):
