5608ed36f9ea89653c13b7c1cde4f133229064b3
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-09-22 20:52:57 +0300

    debuggign

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -60,6 +60,9 @@ class Trainer:
             loss = closure()
             if loss.item() == 0:
                 return loss
+            print(batch)
+            print(type(loss))
+            print(loss, loss.shape)
             loss.backward()
             torch.nn.utils.clip_grad_norm_(self.client.model.parameters(), max_norm=1.0)
             self.client.optimizer.step()
