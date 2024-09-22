5a8924686077f708c832912463d4ccbdfcb8082a
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-09-22 18:46:44 +0300

    only optimize q+proj

--- servers/server_fedit.py
+++ servers/server_fedit.py
@@ -49,7 +49,7 @@ class Server_fedit(BaseServer):
 
         self.config = LoraConfig(
                     r=self.args.r,
-                    target_modules=['q_proj', 'k_proj', 'v_proj', 'o_proj'],
+                    target_modules=['q_proj',],
                     lora_alpha=16,
                     lora_dropout=0.05,
                     bias="none",
