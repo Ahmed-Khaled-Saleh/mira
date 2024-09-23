1c7a6db06ae6fc737d9bda09e7dbe4497505ecd4
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-09-23 19:59:07 +0300

    added mira's configs

---
+++ jobs/mira/mahti/instruct/llama/job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --account=project_2009050
+#SBATCH --job-name=mira_mahti_instruct_llama
+#SBATCH --partition=gpusmall
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=100G
+#SBATCH --time=36:00:00
+#SBATCH --gres=gpu:a100:1
+#SBATCH --output=logs/mira/instruct/llama/out.out
+#SBATCH --error=logs/mira/instruct/llama/out.err
+
+module --force purge
+module load pytorch
+source /projappl/project_2009050/torch/bin/activate
+cd /projappl/project_2009050/code/mira
+
+export PYTHONPATH=$PYTHONPATH:/projappl/project_2009050/torch/lib/python3.9/site-packages
+echo "Current PYTHONPATH: $PYTHONPATH"
+python -c "import rouge; print('rouge module is installed and importable')"
+
+srun python main.py --fname ./configs/mira/instruct/llama/configs.yaml
