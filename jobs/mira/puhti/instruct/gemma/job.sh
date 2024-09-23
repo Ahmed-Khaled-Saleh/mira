fe8fc9167751f65711c2750b111719bc3e2a90a9
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-09-23 20:02:52 +0300

    adjusted job file for puhti mira

--- jobs/mira/puhti/instruct/gemma/job.sh
+++ jobs/mira/puhti/instruct/gemma/job.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #SBATCH --account=project_2009050
-#SBATCH --job-name=puhti_fedit_t5_instruct
-#SBATCH --partition=gpu
+#SBATCH --job-name=mahti_fedit_instruct_t5
+#SBATCH --partition=gpusmall
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=100G
-#SBATCH --time=72:00:00
-#SBATCH --gres=gpu:v100:1
-#SBATCH --output=logs/puhti_fedit_t5_instruct.out
-#SBATCH --error=logs/puhti_fedit_t5_instruct.err
+#SBATCH --time=36:00:00
+#SBATCH --gres=gpu:a100:1
+#SBATCH --output=logs/mahti_fedit_instruct_t5.out
+#SBATCH --error=logs/mahti_fedit_instruct_t5.err
 
 module --force purge
 module load pytorch
-source /projappl/project_2009050/python_envs/torch/bin/activate
+source /projappl/project_2009050/torch/bin/activate
 cd /projappl/project_2009050/code/mira
 
-export PYTHONPATH=$PYTHONPATH:/projappl/project_2009050/python_envs/torch/lib/python3.9/site-packages
+export PYTHONPATH=$PYTHONPATH:/projappl/project_2009050/torch/lib/python3.9/site-packages
 echo "Current PYTHONPATH: $PYTHONPATH"
 python -c "import rouge; print('rouge module is installed and importable')"
 
--- jobs/mira/puhti/instruct/llama/job.sh
+++ jobs/mira/puhti/instruct/llama/job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --account=project_2009050
-#SBATCH --job-name=puhti_fedit_gpt2_instruct
+#SBATCH --job-name=mira_puhti_instruct_llama
 #SBATCH --partition=gpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=100G
-#SBATCH --time=72:00:00
+#SBATCH --time=10:00:00
 #SBATCH --gres=gpu:v100:1
-#SBATCH --output=logs/puhti_fedit_gpt2_instruct.out
-#SBATCH --error=logs/puhti_fedit_gpt2_instruct.err
+#SBATCH --output=logs/mira/instruct/llama/out.out
+#SBATCH --error=logs/mira/instruct/llama/out.err
 
 module --force purge
 module load pytorch
@@ -19,4 +19,4 @@ export PYTHONPATH=$PYTHONPATH:/projappl/project_2009050/python_envs/torch/lib/py
 echo "Current PYTHONPATH: $PYTHONPATH"
 python -c "import rouge; print('rouge module is installed and importable')"
 
-srun python main.py --fname ./configs/fedit/configs_fedit_instruct_gpt2.yaml
+srun python main.py --fname ./configs/mira/instruct/llama/configs.yaml
