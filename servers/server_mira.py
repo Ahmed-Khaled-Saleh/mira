6d2cdb21e5866a03ddba394688629543477a542e
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-09-25 11:49:00 +0300

    fixed model loading

--- servers/server_mira.py
+++ servers/server_mira.py
@@ -128,7 +128,10 @@ class Server_mira(BaseServer):
                     client.model = deepcopy(self.model)
 
                 if os.path.exists(model_path):
-                    client.model.load_state_dict(torch.load(model_path, map_location=self.device))                    
+                    set_peft_model_state_dict(client.model,
+                                              torch.load(model_path, map_location=self.device),
+                                              "default")
+                    # client.model.load_state_dict(torch.load(model_path, map_location=self.device))                    
                 
                 client.model = client.model.to(self.device)
                 
