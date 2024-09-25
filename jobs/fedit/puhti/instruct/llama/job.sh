9605544c46972a8fda2a4adeba72f660c3f982fa
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-09-25 15:24:05 +0300

    fixed configs for puhti fedit

--- jobs/fedit/puhti/instruct/llama/job.sh
+++ jobs/fedit/puhti/instruct/llama/job.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #SBATCH --account=project_2009050
-#SBATCH --job-name=puhti_fedit_gpt2_instruct
+#SBATCH --job-name=fedit_puhti_instruct_llama
 #SBATCH --partition=gpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=100G
-#SBATCH --time=72:00:00
+#SBATCH --time=10:00:00
 #SBATCH --gres=gpu:v100:1
-#SBATCH --output=logs/puhti_fedit_gpt2_instruct.out
-#SBATCH --error=logs/puhti_fedit_gpt2_instruct.err
+#SBATCH --output=logs/fedit/instruct/llama/out.out
+#SBATCH --error=logs/fedit/instruct/llama/out.err
 
 module --force purge
 module load pytorch
 source /projappl/project_2009050/python_envs/torch/bin/activate
-cd /projappl/project_2009050/code/mira
+cd /projappl/project_2009050/code/fedit
 
 export PYTHONPATH=$PYTHONPATH:/projappl/project_2009050/python_envs/torch/lib/python3.9/site-packages
 echo "Current PYTHONPATH: $PYTHONPATH"
 python -c "import rouge; print('rouge module is installed and importable')"
 
-srun python main.py --fname ./configs/fedit/configs_fedit_instruct_gpt2.yaml
+srun python main.py --fname ./configs/fedit/instruct/llama/configs.yaml
