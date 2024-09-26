670fcf6b3eceaaa11e93c7a7f6de83ef87b61fa9
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-09-26 05:20:58 +0300

    fixed path

--- jobs/mira/mahti/dolly/llama/job.sh
+++ jobs/mira/mahti/dolly/llama/job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --account=project_2009050
-#SBATCH --job-name=fedit_mahti_dolly_llama
+#SBATCH --job-name=mira_mahti_dolly_llama
 #SBATCH --partition=gpusmall
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=100G
 #SBATCH --time=36:00:00
 #SBATCH --gres=gpu:a100:1
-#SBATCH --output=logs/fedit/dolly/llama/out.out
-#SBATCH --error=logs/fedit/dolly/llama/out.err
+#SBATCH --output=logs/mira/dolly/llama/out.out
+#SBATCH --error=logs/mira/dolly/llama/out.err
 
 module --force purge
 module load pytorch
@@ -19,4 +19,4 @@ export PYTHONPATH=$PYTHONPATH:/projappl/project_2009050/torch/lib/python3.9/site
 echo "Current PYTHONPATH: $PYTHONPATH"
 python -c "import rouge; print('rouge module is installed and importable')"
 
-srun python main.py --fname ./configs/fedit/dolly/llama/configs.yaml
+srun python main.py --fname ./configs/mira/dolly/llama/configs.yaml
